0fcdb2b690f0cecb4b2c9f9b6506a0f223657837
Hassnain-cs <mhassnain3547@conestogac.on.ca>
2026-02-19 11:38:20 -0500

    Implemented User structure with enum Faction and include guards

--- Queue_Linked_List/user.h
+++ Queue_Linked_List/user.h
@@ -1,1 +1,20 @@
 #pragma once
+#ifndef USER_H
+#define USER_H
+
+// Enumeration to represent the faction of a user
+// Using enum improves readability and prevents invalid faction values
+typedef enum {
+    RED,
+    BLUE,
+    GREEN
+} Faction;
+
+// Structure to represent a user in the game
+typedef struct {
+    char username[11];   // 10 characters + null terminator
+    int level;           // Level ranges from 1 to 60
+    Faction faction;     // RED, BLUE, or GREEN
+} User;
+
+#endif
